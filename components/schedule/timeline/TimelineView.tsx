730f77ffa3f6e87fe098c1798fa7b8492d89c15f
Katsuma Kawamura <katsumakawamura.dev@gmail.com>
2025-08-04 04:15:20 +0900

    タイムラインをCSS Grid化して列ズレ解消、内部の縦スクロールを無効化

--- components/schedule/timeline/TimelineView.tsx
+++ components/schedule/timeline/TimelineView.tsx
@@ -1,9 +1,8 @@
 "use client";
 import TimelineGrid from "./TimelineGrid";
 import TimelineBar from "./TimelineBar";
-import TimelineHeader from "./TimelineHeader";
 import { sampleMembers, sampleSchedules } from "./sampleData";
-import { Schedule } from "@/types/schedule"; // 共通型をインポート
+import { Schedule } from "@/types/schedule";
 import { assignSlots } from "@/utils/scheduleUtils";
 
 export default function TimelineView() {
@@ -18,32 +17,48 @@ export default function TimelineView() {
   const slottedSchedules: Schedule[] = assignSlots(schedules);
 
   return (
-    <div className="relative">
-      <TimelineHeader
-        members={members}
-        memberColumnWidth={memberColumnWidth}
-        onSettingsClick={() => alert("設定ボタンクリック！")}
-      />
-      <div className="flex">
-        <div style={{ width: "48px" }} />
-        <div className="relative">
-          <TimelineGrid
-            startHour={startHour}
-            endHour={endHour}
-            pxPerMinute={pxPerMinute}
-            memberCount={members.length}
-            memberColumnWidth={memberColumnWidth}
-          />
-          {slottedSchedules.map((s) => (
-            <TimelineBar
-              key={s.id}
-              schedule={s}
-              members={members}
+    <div className="overflow-x-auto overflow-y-hidden">
+      <div
+        className="grid min-w-max"
+        style={{
+          gridTemplateColumns: `48px repeat(${members.length}, ${memberColumnWidth}px)`,
+        }}
+      >
+        {/* 1行目: ヘッダー（時間ラベル列 + メンバー名） */}
+        <div></div>
+        {members.map((m) => (
+          <div
+            key={m.id}
+            className="text-center font-semibold text-gray-800 border-r border-gray-300"
+          >
+            {m.name}
+          </div>
+        ))}
+
+        {/* 2行目: タイムライン（時間ラベル + グリッド + バー） */}
+        <div className="col-span-full relative flex">
+          {/* 左端：時間ラベル分のスペース */}
+          <div style={{ width: "48px" }} />
+          {/* タイムライン本体 */}
+          <div className="relative">
+            <TimelineGrid
               startHour={startHour}
+              endHour={endHour}
               pxPerMinute={pxPerMinute}
+              memberCount={members.length}
               memberColumnWidth={memberColumnWidth}
             />
-          ))}
+            {slottedSchedules.map((s) => (
+              <TimelineBar
+                key={s.id}
+                schedule={s}
+                members={members}
+                startHour={startHour}
+                pxPerMinute={pxPerMinute}
+                memberColumnWidth={memberColumnWidth}
+              />
+            ))}
+          </div>
         </div>
       </div>
     </div>
